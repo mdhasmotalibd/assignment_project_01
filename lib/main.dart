357b4c74a4e947f97c4372b4cd1940976ac25bcd
mdhasmotalibd <hshasmotbd@gmail.com>
2024-01-16 17:09:51 +0600

    first edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,270 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApps());
+
+class MyApps extends StatefulWidget {
+  MyApps({super.key});
+
+  @override
+  State<MyApps> createState() => _MyAppsState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppsState extends State<MyApps> {
+  List<CartItem> cartItems = [
+    CartItem(
+        name: 'Pullover',
+        color: 'Black',
+        price: 51,
+        quantity: 0,
+        size: 'L',
+        image: 'assets/pullover.jpg'),
+    CartItem(
+        name: 'T-Shirt',
+        color: 'Gray',
+        price: 30,
+        quantity: 0,
+        size: 'L',
+        image: 'assets/tshirt.jpg'),
+    CartItem(
+        name: 'Sport Dress',
+        color: 'Blue',
+        price: 43,
+        quantity: 0,
+        size: 'M',
+        image: 'assets/sportdress.jpg'),
+  ];
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(title: Text('My Bag'),
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    size: 30,
+                  ))
+            ],
+          ),
+          body:Column(children: [ Expanded(flex: 6,
+            child: ListView.builder(
+                itemCount: cartItems.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 1,
+                              blurRadius: 8)
+                        ]),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 18, left: 6),
+                          child: Image.asset(
+                            cartItems[index].image,
+                            height: 135,
+                            width: 110,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(
+                                  bottom: 2,
+                                ),
+                                child: Text(
+                                  cartItems[index].name,
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(bottom: 12),
+                                child: Text(
+                                  "Colors:${cartItems[index].color} Size:${cartItems[index].size}",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.all(4),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(20),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: Colors.grey.withOpacity(0.4),
+                                            spreadRadius: 2,
+                                            blurRadius: 6),
+                                      ],
+                                    ),
+                                    child: GestureDetector(
+                                      child: Icon(CupertinoIcons.minus),
+                                      onTap: () {
+                                        setState(() {
+                                          if (cartItems[index].quantity > 0) {
+                                            cartItems[index].quantity--;
+                                          }
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  Container(
+                                    child: Padding(
+                                      padding:
+                                      EdgeInsets.symmetric(horizontal: 10),
+                                      child: Text(cartItems[index].quantity.toString(),
+                                          style: TextStyle(
+                                              fontSize: 24,
+                                              fontWeight: FontWeight.bold)),
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.all(4),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(20),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: Colors.grey.withOpacity(0.4),
+                                            spreadRadius: 2,
+                                            blurRadius: 6),
+                                      ],
+                                    ),
+                                    child: GestureDetector(
+                                      child: Icon(CupertinoIcons.plus),
+                                      onTap: () {
+                                        setState(() {
+                                          cartItems[index].quantity++;
+                                          if (cartItems[index].quantity == 5) {
+                                            showCongratulationsDialog();
+                                          }
+                                        });
+                                      },
+                                    ),
+                                  )
+                                ],
+                              )
+                            ]),
+                        Spacer(),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Icon(
+                                Icons.more_vert,
+                                color: Colors.grey,
+                                size: 30,
+                              ),
+                              SizedBox(height: 50),
+                              Container(
+                                child: Text("${cartItems[index].price}\$",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                    )),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+
+                }),
+          ),
+            Padding(
+              padding:EdgeInsets.only(bottom: 70),
+              child: SizedBox(width: 370,
+                child: Row(
+                  children:[
+                    Text(
+                      'Total amount : ',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                    Spacer(),
+                    Text(
+                      '${calculateTotalPrice().toString()}\$',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+                  ],
+                ),
+              ),
+            )
+          ]),
+          //backgroundColor: Colors.green,
+          floatingActionButton: SizedBox(
+            width: 380,
+            height: 50,
+            child: FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: Colors.red[600],
+              child: Text('CHECK OUT',
+                  style: TextStyle(color: Colors.white, fontSize: 20)),
+            ),
+          ),
+        )
+
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int calculateTotalPrice() {
+    int total = 0;
+    for (CartItem item in cartItems) {
+      total += item.price * item.quantity;
+    }
+    return total;
   }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+  void showCongratulationsDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Congratulations!'),
+          content: Text('You have added five items. Well done!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
     );
   }
+}
+
+class CartItem {
+  String name;
+  int price;
+  int quantity;
+  String color;
+  String image;
+  String size;
+
+  CartItem(
+      {required this.name,
+        required this.price,
+        required this.quantity,
+        required this.color,
+        required this.image,
+        required this.size});
 }
